4866e9d50b23b9e1c23242ffd87debc819ba30a7
Shami R Nayak <shaminayak2004@gmail.com>
2026-02-27 23:17:33 +0530

    Add files via upload

---
+++ conditional.py
@@ -0,0 +1,8 @@
+a=int((input("enter a num")))
+b=int(input("enter a num"))
+if a>b:
+    print("a is greater than b")
+elif b>a:
+    print("b is greater than a")
+else:
+    print("both are equal")

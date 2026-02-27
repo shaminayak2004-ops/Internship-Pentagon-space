d5f8d03b18ccf8b32cfe2b9f5e523ac693bd41d5
Shami R Nayak <shaminayak2004@gmail.com>
2026-02-27 23:17:08 +0530

    Add files via upload

---
+++ looping.py
@@ -0,0 +1,20 @@
+#There are 2 looping statements for,while
+for i in 1,2,3,4:
+    print(i)          
+
+for i in "rahul","ramya":
+    print(i)
+
+#range function:
+#range(stop)
+#range(start,stop)
+#range(start,stop,step)
+#start default value is 0,step=1, in stop last values are excluded
+for i in range(100):
+    print(i)
+#while loop
+#imp to execute: intialization and increment
+i=0
+while i<=2:
+    print("hello") 
+    i=i+1
